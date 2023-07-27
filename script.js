79a472932a82ad2ed6675c2ceb51d42706848486
Samuel Santos <mucasantos@gmail.com>
2023-07-26 21:55:04 -0300

    Criar js

---
+++ script.js
@@ -0,0 +1,28 @@
+/*
+Git básico
+Introdução Javascript
+
+*/
+
+var nome = "Samuel"
+let idade = 45
+const anoNascimento = 1977
+let idadeEsposa = "45"
+let idadeString = "45"
+/*
+= atribuição
+== comparar se é igual
+=== comparar se é exatamente igual
+*/
+
+let listaCompras = []
+
+listaCompras.push("Maça")
+
+
+console.log(listaCompras.length)
+console.log(listaCompras)
+
+
+
+
