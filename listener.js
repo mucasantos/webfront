6c38521a0fdd76aad79d420fa876a001ea8dc9b1
Samuel Santos <mucasantos@gmail.com>
2023-08-02 19:57:20 -0300

    Aula innerHTML

--- listener.js
+++ listener.js
@@ -1,13 +1,31 @@
+//Criar um componente HTML
+//usar o innerHTML
+//Usar a concatenação de Javascript
+
 const titulo = document.getElementById("titulo")
 const minhaDiv = document.getElementById("minha-div")
+const buttonAdd = document.getElementById("button")
+const userName = document.getElementById("nome")
+
+let idade = 26
+
+buttonAdd.addEventListener('click', () => {
+
+
+    minhaDiv.innerHTML = `
+    <h2>${idade}</h2>
+    <button>Pode clicar...</button>
+    `
+
+})
 
 
 minhaDiv.addEventListener(
-   "click", function () {
+    "click", function () {
         titulo.textContent = "Mudei mesmo!!"
         titulo.style.color = "yellow"
         titulo.style.backgroundColor = "red"
-        minhaDiv.style.borderRadius ="10px"
+        minhaDiv.style.borderRadius = "10px"
     }
 )
 
