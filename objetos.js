baead25c32494a652ed8ee50bf823200fcac08e8
Samuel Santos <mucasantos@gmail.com>
2023-08-08 20:44:52 -0300

    Objetos Javascript: parte1 - add metodo

--- objetos.js
+++ objetos.js
@@ -8,7 +8,7 @@ const button = document.getElementById("btn")
 
 const person = { 
     name: "Samuel Santos", 
-    age: 45, 
+    born: 1977, 
     weight: "98kb", 
     gender: "Mininu",
     height: 1.83,
@@ -16,29 +16,43 @@ const person = {
         wife: "Dora Santos",
         son: "Felipe Santos",
         son2: "Luis Antônio"
-    },
+    }, 
     cars: ["Dodge","Fit", "Hillux", "BMW", "Ferrari"],
     work: {
         company: "Senai SP",
         address: "Rua Direita, 222",
         ocuppation: "Instrutor PIII",
         salary: "R$116.235,89"
-    }, 
+    },
+    calcAge: function () {
+        const currentData = new Date(Date.now())
+        return currentData.getFullYear() - this.born       
+    }
+
 }
+
+
 
 let produto = {}
 let produtos = []
 
 button.addEventListener('click', ()=>{
-
     produto = {
+        //Gera id randomico e arredonda
         id: Math.floor(Math.random() * 21),
         name: prod.value,
         color: color.value
     }
-    
+    //método para adicionar dado em uma lista
     produtos.push(produto)
     console.log(produtos)
+
+    console.log(person.calcAge())
+   
+   
+   
+
+
 
 })
 
