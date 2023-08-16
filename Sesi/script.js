9d6c63b1bc23955a7950257d6d8e05932bc0aa5f
Samuel Santos <mucasantos@gmail.com>
2023-08-16 19:59:51 -0300

    aula addEventListener - listas - forEach turma Noite 16/08

--- Sesi/script.js
+++ Sesi/script.js
@@ -3,29 +3,67 @@ Review:
 DOM: 
 - addEventListener
 - textContent - OK
-
 Variáveis - OK
 For / if - OK
 Function - OK
-
 */
+//Percorrendo listas
+/* const listaCores = [
+    { cor: 'blue', local: 'Brazil' }, //elemento 0
+    { cor: 'green', local: 'England' } // elemento 1
+]
+
+listaCores.forEach(element => {
+    console.log(element.local)
+}) */
 
 const minhaDiv = document.getElementById("minha-div")
+//ME devolve uma lista --> elementos html
 const buttons = document.querySelectorAll('button')
 
 //forEach na lista de botoes, como melhoria de código
 // ao invés de criar uma const para cada id
 
 buttons.forEach(element => {
-    element.addEventListener('click', ()=> {
+    element.addEventListener('click', () => {
         minhaDiv.style.backgroundColor = element.id
-    }) 
+    })
 })
 
-minhaDiv.addEventListener('mouseover', ()=> {
-    minhaDiv.style.backgroundColor = 'pink'  
-})
+/**Utilizando o createElement e o appendChild, elimine 
+ * os botões do HTML, criando-os de forma dinâmica
+ * a partir de uma lista de cores (Cores em inglês).
+ * 
+ */
+/*
+var px = 0;
+var py = 0;
 
+document.addEventListener('keydown', (event) => {
+    var tecla = event.key;
+
+
+    console.log(tecla)
+    if (tecla == "ArrowLeft") {
+        //Esquerda
+        px -= 10;
+        minhaDiv.style.left = px + "px";
+    } else if (tecla == "ArrowRight") {
+        //Direita
+        px += 10;
+        minhaDiv.style.left = px + "px";
+    }
+    if (tecla == "ArrowUp") {
+        //Cima
+        py -= 10;
+        minhaDiv.style.top = py + "px";
+    } else if (tecla == "ArrowDown") {
+        //baixo
+        py += 10;
+        minhaDiv.style.top = py + "px";
+    }
+})
+*/
 //addEventListener -> Ele precisa de 2 parâmentros: a ação e a função que será executada
 
 /**
