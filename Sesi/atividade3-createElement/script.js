18d2fa8f8603855ca8aae52cba68f33863e9333d
Samuel Santos <mucasantos@gmail.com>
2023-09-20 19:35:39 -0300

    Atividade clima-tempo

--- Sesi/atividade3-createElement/script.js
+++ Sesi/atividade3-createElement/script.js
@@ -1,104 +1,233 @@
-
-//elemento pai para inserir os cards
-const elementoDad = document.getElementById('container')
-
-//objeto card 
+const myDad = document.getElementById("dad");
+//Meu Objeto Javascript para representar o meu
+//card do html
+const cardWeather = {
+  image: "imagens/londres.jpg",
+  city: "LONDON",
+  temp: "60&deg",
+  listWeather: [
+    {
+      day: "MON",
+      image: "imagens/img_weather_cloud.jpg",
+    },
+    {
+      day: "TUE",
+      image: "imagens/img_weather_clouds.jpg",
+    },
+    {
+      day: "WED",
+      image: "imagens/img_weather_sun.jpg",
+    },
+  ],
+};
 
 const cards = [
   {
-    imagem: "imagens/londres.jpg",
-    city: "Paraiba",
-    temp: "60 F",
-    days: ['DOM', 'SEG', 'TER'],
-    icons: ['imagens/moon.png', 'imagens/moon2.png', 'imagens/rain.png'],
-    report: [
+    image: "imagens/londres.jpg",
+    city: "LONDON",
+    temp: "60&deg",
+    listWeather: [
       {
-        day: 'MON',
-        icon: 'imagens/moon.png'
-      }
-    ]
-  },
-  {
-    imagem: "imagens/img_london.jpg",
-    city: "London",
-    temp: "60 F",
-    days: ['MON', 'TUE', 'WED'],
-    icons: ['imagens/moon.png', 'imagens/moon2.png', 'imagens/rain.png'],
-    report: [
-      {
-        day: 'MON',
-        icon: 'imagens/moon.png'
-      }
-    ]
-  },
-  {
-    imagem: "imagens/img_london.jpg",
-    city: "London",
-    temp: "60 F",
-    days: ['MON', 'TUE', 'WED'],
-    icons: ['imagens/moon.png', 'imagens/moon2.png', 'imagens/rain.png'],
-    report: [
-      {
-        day: 'MON',
-        icon: 'imagens/moon.png'
-      }
-    ]
-  },
-  {
-    imagem: "imagens/img_london.jpg",
-    city: "London",
-    temp: "60 F",
-    days: ['MON', 'TUE', 'WED'],
-    icons: ['imagens/moon.png', 'imagens/moon2.png', 'imagens/rain.png'],
-    report: [
-      {
-        day: 'MON',
-        icon: 'imagens/moon1.png'
+        day: "MON",
+        image: "imagens/img_weather_cloud.jpg",
       },
       {
-        day: 'MON',
-        icon: 'imagens/moon.png'
+        day: "TUE",
+        image: "imagens/img_weather_clouds.jpg",
       },
       {
-        day: 'MON',
-        icon: 'imagens/rain.png'
-      }
-    ]
+        day: "WED",
+        image: "imagens/img_weather_sun.jpg",
+      },
+    ],
   },
-]
+  {
+    image: "imagens/londres.jpg",
+    city: "PARAIBA",
+    temp: "60&deg",
+    listWeather: [
+      {
+        day: "MON",
+        image: "imagens/img_weather_cloud.jpg",
+      },
+      {
+        day: "TUE",
+        image: "imagens/img_weather_clouds.jpg",
+      },
+      {
+        day: "WED",
+        image: "imagens/img_weather_sun.jpg",
+      },
+    ],
+  },
+  {
+    image: "imagens/londres.jpg",
+    city: "BAHIA",
+    temp: "60&deg",
+    listWeather: [
+      {
+        day: "MON",
+        image: "imagens/img_weather_cloud.jpg",
+      },
+      {
+        day: "TUE",
+        image: "imagens/img_weather_clouds.jpg",
+      },
+      {
+        day: "WED",
+        image: "imagens/img_weather_sun.jpg",
+      },
+    ],
+  },
+  {
+    image: "imagens/londres.jpg",
+    city: "CARUARU",
+    temp: "60&deg",
+    listWeather: [
+      {
+        day: "MON",
+        image: "imagens/img_weather_cloud.jpg",
+      },
+      {
+        day: "TUE",
+        image: "imagens/img_weather_clouds.jpg",
+      },
+      {
+        day: "WED",
+        image: "imagens/img_weather_sun.jpg",
+      },
+    ],
+  },
+  {
+    image: "imagens/londres.jpg",
+    city: "COTIA",
+    temp: "60&deg",
+    listWeather: [
+      {
+        day: "MON",
+        image: "imagens/img_weather_cloud.jpg",
+      },
+      {
+        day: "TUE",
+        image: "imagens/img_weather_clouds.jpg",
+      },
+      {
+        day: "WED",
+        image: "imagens/img_weather_sun.jpg",
+      },
+    ],
+  },
+  {
+    image: "imagens/londres.jpg",
+    city: "BARUERI",
+    temp: "60&deg",
+    listWeather: [
+      {
+        day: "MON",
+        image: "imagens/img_weather_cloud.jpg",
+      },
+      {
+        day: "TUE",
+        image: "imagens/img_weather_clouds.jpg",
+      },
+      {
+        day: "WED",
+        image: "imagens/img_weather_sun.jpg",
+      },
+    ],
+  },
+  {
+    image: "imagens/londres.jpg",
+    city: "CAUCAIA",
+    temp: "60&deg",
+    listWeather: [
+      {
+        day: "MON",
+        image: "imagens/img_weather_cloud.jpg",
+      },
+      {
+        day: "TUE",
+        image: "imagens/img_weather_clouds.jpg",
+      },
+      {
+        day: "WED",
+        image: "imagens/img_weather_sun.jpg",
+      },
+    ],
+  },
+  {
+    image: "imagens/londres.jpg",
+    city: "RIO DE JANEIRO",
+    temp: "60&deg",
+    listWeather: [
+      {
+        day: "MON",
+        image: "imagens/img_weather_cloud.jpg",
+      },
+      {
+        day: "TUE",
+        image: "imagens/img_weather_clouds.jpg",
+      },
+      {
+        day: "WED",
+        image: "imagens/img_weather_sun.jpg",
+      },
+    ],
+  },
+  {
+    image: "imagens/londres.jpg",
+    city: "CARAPICUIBA",
+    temp: "60&deg",
+    listWeather: [
+      {
+        day: "MON",
+        image: "imagens/img_weather_cloud.jpg",
+      },
+      {
+        day: "TUE",
+        image: "imagens/img_weather_clouds.jpg",
+      },
+      {
+        day: "WED",
+        image: "imagens/img_weather_sun.jpg",
+      },
+    ],
+  },
+];
 
-/*
-for (let index = 0; index < cards.length; index++) {
-  const element = cards[index];
+//for
+
+cards.forEach((card) => {
+  const littleBoy = document.createElement("div");
+
+  //Estou adicionando o nome das classes a minha div
+  littleBoy.classList.add("w3-card-4")
+  littleBoy.classList.add("w3-margin")
+  littleBoy.style.width = "40%"
   
-}
-*/
-
-cards.forEach(element => {
-  const mySon = document.createElement("div")
-  mySon.classList.add('w3-card-4')
-  mySon.classList.add('w3-margin')
-  mySon.style.width = '40%'
-
-  mySon.innerHTML = `
+//Finalizar o código abaixo, com os dados vindo da lista
+  littleBoy.innerHTML = `
+  
   <div class="w3-display-container w3-text-white">
-    <img src="${element.imagem}" alt="Lights" style="width:100%">
-    <div class="w3-xlarge w3-display-bottomleft w3-padding">${element.city} 60&deg; F</div>
+    <img src="${card.image}" alt="Lights" style="width:100%">
+    <div class="w3-xlarge w3-display-bottomleft w3-padding">${card.city} ${card.temp} F</div>
   </div>
   <div class="w3-row">
     <div class="w3-third w3-center">
-      <h3>${element.report[0].day}</h3>
-      <img src="${element.report[0].icon}" alt="sun" style="width:80px">
+      <h3>${card.listWeather[0].day}</h3>
+      <img src="${card.listWeather[0].image}" alt="sun" style="width:80px">
     </div>
     <div class="w3-third w3-center">
-      <h3>${element.days[1]}</h3>
-      <img src="${element.icons[1]}" alt="cloud" style="width:80px">
+      <h3>${card.listWeather[1].day}</h3>
+      <img src="${card.listWeather[1].image}" alt="cloud" style="width:80px">
     </div>
     <div class="w3-third w3-center w3-margin-bottom">
-      <h3>${element.days[2]}</h3>
-      <img src="${element.icons[2]}" alt="clouds" style="width:80px">
+      <h3>${card.listWeather[2].day}</h3>
+      <img src="${card.listWeather[2].image}" alt="clouds" style="width:80px">
     </div>
-  </div>`
+  </div>
 
-  elementoDad.appendChild(mySon)
-})
+    
+  `
+  myDad.appendChild(littleBoy)
+});
